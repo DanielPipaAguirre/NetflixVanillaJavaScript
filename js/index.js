b95bdb6e70440b9d8a3c278851fe3f4722faaf17
Daniel Pipa <danielpipa21@gmail.com>
2020-03-04 14:57:38 -0500

    Modal & Animation - JS

--- js/index.js
+++ js/index.js
@@ -28,17 +28,55 @@
         const templateHTML = createTemplate(pelis[0]);
         feature.innerHTML = templateHTML;
     })
-    const actionList = await getData(`${BASE_API}list_movies.json?genre=action`);
-    const dramaList = await getData(`${BASE_API}list_movies.json?genre=action`);
-    function loadList(list, render) {
+    function findByID(list, id) {
+        return list.find(movie => movie.id === parseInt(id, 10))
+    }
+    function findMovie(id, cat) {
+        switch (cat) {
+            case 'drama': {
+                return findByID(dramaList, id)
+            }
+            case 'action': {
+                return findByID(actionList, id)
+            }
+        }
+    }
+    function addEventClick(element) {
+        element.addEventListener('click', () => {
+            overlay.classList.add("active");
+            modal.style.animation = "modalIn .8s forwards"
+            const id = element.dataset.id;
+            const cat = element.dataset.cat;
+            const data = findMovie(id, cat);
+            modal.querySelector('h1').textContent = `${data.title}`;
+            modal.querySelector('.modal__description img').setAttribute('src', `${data.medium_cover_image}`);
+            modal.querySelector('.modal__description p').textContent = `${data.description_full}`;
+        })
+    }
+    modalClose.addEventListener("click", () => {
+        modal.style.animation = "modalOut .8s forwards"
+        setTimeout(() => {
+            overlay.classList.remove("active");
+        }, 800);
+    })
+    function loadList(list, render, category) {
         render.children[0].remove();
-        list.data.movies.forEach(movie => {
+        list.forEach(movie => {
             const div = document.createElement('div');
             div.innerHTML = `<img src="${movie.medium_cover_image}" alt="">`;
+            div.setAttribute("data-id", `${movie.id}`);
+            div.setAttribute("data-cat", `${category}`)
             render.appendChild(div);
+            /* const images = div.querySelector('img');
+            images.addEventListener("load", e => {
+                e.srcElement.classList.add("fadeIn");
+            }) */
+            addEventClick(div);
         });
     }
-    loadList(actionList, series);
-    loadList(dramaList, movies);
+    const { data: { movies: actionList } } = await getData(`${BASE_API}list_movies.json?genre=action`);
+    loadList(actionList, series, "action");
+    const { data: { movies: dramaList } } = await getData(`${BASE_API}list_movies.json?genre=drama`);
+    loadList(dramaList, movies, "drama");
 })()
 
